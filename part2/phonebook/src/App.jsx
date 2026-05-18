3c60796ea2c0e51f5eeeae9f63a14808cd8ab29d
Alex <alexn7295@gmail.com>
2026-05-18 19:12:00 -0400

    Phonebook 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -12,10 +12,16 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
+    
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook`)
+      return
+    }
+
     const personObject = {
       name: newName
     }
-    
+
     setPersons(persons.concat(personObject))
     setNewName('')
   }
