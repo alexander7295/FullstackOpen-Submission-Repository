fd56c178522d718e4d7651f1db5728a29ef901e5
Alex <alexn7295@gmail.com>
2026-05-19 18:05:29 -0400

    Phonebook 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -44,9 +44,11 @@ const App = () => {
       number: newNumber
     }
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+    axios.post('http://localhost:3001/persons', personObject).then(response => {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+    })
   }
 
   const handleNameFilterChange = (event) => {
