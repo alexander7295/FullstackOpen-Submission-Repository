d8ce9e1ba0722670661892e05f55f3146a6a1ce5
Alex <alexn7295@gmail.com>
2026-05-21 00:49:42 -0400

    Added middleware express static

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -8,6 +8,7 @@ morgan.token('body', (request) => {
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 let persons = [
     { 
