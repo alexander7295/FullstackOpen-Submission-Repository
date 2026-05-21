16d895a403554b41ceebe9a0e1dc074a55e0dd7f
Alex <alexn7295@gmail.com>
2026-05-21 19:52:59 -0400

    Phonebook database 3.15

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -65,9 +65,12 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
+    // persons = persons.filter(person => person.id !== id)
 
-    response.status(204).end()
+    // response.status(204).end()
+    Person.findByIdAndDelete(id).then(result => {
+        response.status(204).end()
+    })
 })
 
 app.post('/api/persons', (request, response) => {
