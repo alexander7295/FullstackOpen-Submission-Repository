0897a080ae1e9024035a661cf994e6af32275f8b
Alex <alexn7295@gmail.com>
2026-05-25 16:37:08 -0400

    Blog list 4.4

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -8,3 +8,58 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const biggerList = [
+    {
+      _id: '1a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '2a422aa71b54a676234d17f8',
+      title: 'abc',
+      author: 'abc',
+      url: 'url1',
+      likes: 20,
+      __v: 0
+    },
+    {
+      _id: '3a422aa71b54a676234d17f8',
+      title: 'aaa',
+      author: 'aaa',
+      url: 'url2',
+      likes: 30,
+      __v: 0
+    }
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(biggerList)
+    assert.strictEqual(result, 60)
+  })
+})
