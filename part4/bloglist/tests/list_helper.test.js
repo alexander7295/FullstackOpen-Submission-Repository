cf7f40997d2ed8ab0bc2f71f006e9e0c3e1680fe
Alex <alexn7295@gmail.com>
2026-05-25 17:01:26 -0400

    Blog list 4.5

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -9,45 +9,37 @@ test('dummy returns one', () => {
   assert.strictEqual(result, 1)
 })
 
+const list1 = {
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'Go To Statement Considered Harmful',
+  author: 'Edsger W. Dijkstra',
+  url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+  likes: 10,
+  __v: 0
+}
+  
+const list2 = {
+  _id: '2a422aa71b54a676234d17f8',
+  title: 'abc',
+  author: 'abc',
+  url: 'url1',
+  likes: 20,
+  __v: 0
+}
+
+const list3 = {
+  _id: '3a422aa71b54a676234d17f8',
+  title: 'aaa',
+  author: 'aaa',
+  url: 'url2',
+  likes: 30,
+  __v: 0
+}
+
+const listWithOneBlog = [list1]
+const biggerList = [list1, list2, list3]
+
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  const biggerList = [
-    {
-      _id: '1a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 10,
-      __v: 0
-    },
-    {
-      _id: '2a422aa71b54a676234d17f8',
-      title: 'abc',
-      author: 'abc',
-      url: 'url1',
-      likes: 20,
-      __v: 0
-    },
-    {
-      _id: '3a422aa71b54a676234d17f8',
-      title: 'aaa',
-      author: 'aaa',
-      url: 'url2',
-      likes: 30,
-      __v: 0
-    }
-  ]
-
   test('of empty list is zero', () => {
     const result = listHelper.totalLikes([])
     assert.strictEqual(result, 0)
@@ -55,11 +47,28 @@ describe('total likes', () => {
 
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
-    assert.strictEqual(result, 5)
+    assert.strictEqual(result, 10)
   })
 
   test('of a bigger list is calculated right', () => {
     const result = listHelper.totalLikes(biggerList)
     assert.strictEqual(result, 60)
+  })
+})
+
+describe('favorite blog', () => {
+  test('of an empty list is null', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.deepStrictEqual(result, null)
+  })
+
+  test('of a list with one blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.deepStrictEqual(result, list1)
+  })
+
+  test('of a bigger list with multiple blogs', () => {
+    const result = listHelper.favoriteBlog(biggerList)
+    assert.deepStrictEqual(result, list3)
   })
 })
